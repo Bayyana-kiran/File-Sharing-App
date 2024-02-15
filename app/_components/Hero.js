808c3393cd34a9a0362700a39a27f7a19aee1211
Bayyana-kiran <bayyanakiran.gmail.com>
2024-02-16 01:44:46 +0530

    Done with landing page

--- app/_components/Hero.js
+++ app/_components/Hero.js
@@ -1,43 +1,29 @@
-import React from 'react'
+import React from "react";
+import Constant from "../_utils/Constant";
 
 function hero() {
   return (
     <div>
-      {/*
-  Heads up! 👋
-
-  This component comes with some `rtl` classes. Please remove them if they are not needed in your project.
-*/}
-
-      <section className="relative bg-[url(https://images.unsplash.com/photo-1604014237800-1c9102c219da?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1770&q=80)] bg-cover bg-center bg-no-repeat">
-        <div className="absolute inset-0 bg-white/75 sm:bg-transparent sm:from-white/95 sm:to-white/25 ltr:sm:bg-gradient-to-r rtl:sm:bg-gradient-to-l"></div>
-
-        <div className="relative mx-auto max-w-screen-xl px-4 py-32 sm:px-6 lg:flex lg:h-screen lg:items-center lg:px-8">
-          <div className="max-w-xl text-center ltr:sm:text-left rtl:sm:text-right">
+      <section className="bg-gray-50">
+        <div className="mx-auto max-w-screen-xl px-4 py-32 lg:flex lg:h-screen lg:items-center">
+          <div className="mx-auto max-w-xl text-center">
             <h1 className="text-3xl font-extrabold sm:text-5xl">
-              Let us find your
-              <strong className="block font-extrabold text-rose-700">
-                {" "}
-                Forever Home.{" "}
-              </strong>
+              Upload and Sharing Documents in Secured manner
             </h1>
 
-            <p className="mt-4 max-w-lg sm:text-xl/relaxed">
-              Lorem ipsum dolor sit amet consectetur, adipisicing elit. Nesciunt
-              illo tenetur fuga ducimus numquam ea!
-            </p>
+            <p className="mt-4 sm:text-xl/relaxed text-gray">{Constant.desc}</p>
 
-            <div className="mt-8 flex flex-wrap gap-4 text-center">
+            <div className="mt-8 flex flex-wrap justify-center gap-4">
               <a
+                className="block w-full rounded bg-primary px-12 py-3 text-sm font-medium text-white shadow hover:bg-slate-950 focus:outline-none focus:ring active:bg-red-500 sm:w-auto"
                 href="#"
-                className="block w-full rounded bg-rose-600 px-12 py-3 text-sm font-medium text-white shadow hover:bg-rose-700 focus:outline-none focus:ring active:bg-rose-500 sm:w-auto"
               >
                 Get Started
               </a>
 
               <a
+                className="block w-full rounded px-12 py-3 text-sm font-medium text-primary shadow hover:text-slate-950 focus:outline-none focus:ring active:text-red-500 sm:w-auto"
                 href="#"
-                className="block w-full rounded bg-white px-12 py-3 text-sm font-medium text-rose-600 shadow hover:text-rose-700 focus:outline-none focus:ring active:text-rose-500 sm:w-auto"
               >
                 Learn More
               </a>
@@ -49,4 +35,4 @@ function hero() {
   );
 }
 
-export default hero
+export default hero;
---
+++ app/_utils/Constant.js
@@ -0,0 +1,3 @@
+export default {
+    desc:'Upload the files in secure'
+}
